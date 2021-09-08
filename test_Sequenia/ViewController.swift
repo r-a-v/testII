558e5d42e19e30f9b11730dbfd94b71d7b8776fb
Alexander Rudikov <rav.lnk1991@gmail.com>
2021-09-08 14:47:20 +0700

    Initial Commit

---
+++ test_Sequenia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_Sequenia
+//
+//  Created by aleksandr_rudikov on 08.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
